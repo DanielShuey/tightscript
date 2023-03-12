e8f3fb723b8098df3498f4d498a55f4ca2871436
Daniel Shuey <>
2023-03-12 23:21:59 +1100

    try again

--- src/chain-of-responsibility.ts
+++ src/chain-of-responsibility.ts
@@ -26,8 +26,7 @@ namespace ChainOfResponsibility {
 
     async then<T2>(func: (prev: T) => Promise<T2>): Promise<Handler<T2>> {
       try {
-        const result: T2 = await func(this.prev);
-        return new Handler<T2>(result);
+        return new Handler<T2>(await func(await this.prev));
       } catch (e: any) {
         throw new Error(e.message);
       }
