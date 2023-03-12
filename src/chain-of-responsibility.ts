d898641b1225564535458ee916c971fc9db2b1fd
Daniel Shuey <>
2023-03-12 23:11:50 +1100

    try again

--- src/chain-of-responsibility.ts
+++ src/chain-of-responsibility.ts
@@ -10,21 +10,18 @@ namespace ChainOfResponsibility {
       this.prev = prev;
     }
 
-    public result(): T {
+    result(): T {
       return this.prev;
     }
 
-    public reject(
-      predicate: (prev: T) => boolean,
-      message: string
-    ): Handler<T> {
+    reject(predicate: (prev: T) => boolean, message: string): Handler<T> {
       if (predicate(this.prev)) {
         throw new Error(message);
       }
       return this;
     }
 
-    public async then<T2>(func: (prev: T) => T2): Promise<Handler<T2>> {
+    async then<T2>(func: (prev: T) => Promise<T2>): Promise<T2> {
       try {
         const result: T2 = await func(this.prev);
         return new Handler<T2>(result);
